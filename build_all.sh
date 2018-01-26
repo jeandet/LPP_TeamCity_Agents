fb1f5ffba9107929dd22ab4fbc43e141886f740c
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2018-01-26 17:54:16 +0100

    Added a build script to build all images
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

---
+++ build_all.sh
@@ -0,0 +1,6 @@
+repo_list=${teamcity-docker-minimal-agent teamcity-docker-complete-agent teamcity-docker-SolarOrbiter-LFR-agent teamcity-docker-agent-debian-stable teamcity-docker-agent-osx teamcity-docker-agent-freebsd}
+
+
+for d in $repo_list; do
+    cd $d && sh build_image.sh && cd ..
+done
