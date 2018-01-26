45e887ecd70adccf6972302b7b00dfc4e6363ac0
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2018-01-26 20:35:59 +0100

    FIxed mistake in build all bash script
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- build_all.sh
+++ build_all.sh
@@ -1,6 +1,11 @@
-repo_list=${teamcity-docker-minimal-agent teamcity-docker-complete-agent teamcity-docker-SolarOrbiter-LFR-agent teamcity-docker-agent-debian-stable teamcity-docker-agent-osx teamcity-docker-agent-freebsd}
+repo_list="teamcity-docker-minimal-agent
+teamcity-docker-complete-agent
+teamcity-docker-SolarOrbiter-LFR-agent
+teamcity-docker-agent-debian-stable
+teamcity-docker-agent-osx
+teamcity-docker-agent-freebsd"
 
 
-for d in $repo_list; do
+for d in ${repo_list}; do
     cd $d && sh build_image.sh && cd ..
 done
