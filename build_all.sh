b5914098a59bea75a796091b9689b4ffc5d18cac
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2019-09-19 19:09:29 +0200

    Fix typo and changed registry
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- build_all.sh
+++ build_all.sh
@@ -2,14 +2,7 @@
 repo_list="teamcity-docker-minimal-agent
 teamcity-docker-sciqlop-fedora
 teamcity-docker-agent-fedora-lppic
-teamcity-docker-in-docker
-teamcity-docker-VM-base-agent
-teamcity-docker-agent-ubuntu-17-10
 teamcity-docker-complete-agent
-teamcity-docker-SolarOrbiter-LFR-agent
-teamcity-docker-agent-debian-stable
-teamcity-docker-agent-osx
-teamcity-docker-agent-freebsd
 teamcity-docker-agent-fedora-HPC"
 
 
@@ -19,6 +12,7 @@ git submodule update
 for d in ${repo_list}; do
     cd $d && bash build_image.sh && cd ..
     image_name=$(grep "docker build " $d/build_image.sh | rev | cut -d ' ' -f 2 | rev)
-    docker tag $image_name pc-solar5:5000/$image_name
-    docker push pc-solar5:5000/$image_name
+    docker tag $image_name kube-master:32219/$image_name
+    docker push kube-master:32219/$image_name
 done
+
