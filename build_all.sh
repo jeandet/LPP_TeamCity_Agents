6bdd7289fc790a7722394a9491e736200c711530
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2020-11-27 16:36:44 +0100

    Update registry URL
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- build_all.sh
+++ build_all.sh
@@ -5,6 +5,7 @@ teamcity-docker-agent-fedora-lppic
 teamcity-docker-complete-agent
 teamcity-docker-agent-fedora-HPC"
 
+REGISTRY="129.104.6.165:32219"
 
 git submodule init
 git submodule update
@@ -12,7 +13,7 @@ git submodule update
 for d in ${repo_list}; do
     cd $d && bash build_image.sh && cd ..
     image_name=$(grep "docker build " $d/build_image.sh | rev | cut -d ' ' -f 2 | rev)
-    docker tag $image_name kube-master:32219/$image_name
-    docker push kube-master:32219/$image_name
+    docker tag $image_name $REGISTRY/$image_name
+    docker push $REGISTRY/$image_name
 done
 
